ef3579de3f0e4c2fd3e7791ba9e64b583c8d91af
javiernovoa <55000848+javiernovoa@users.noreply.github.com>
2019-12-04 20:03:12 -0300

    Vamos shile que se puede

---
+++ HomeAddvisor/Controllers/LoginController.cs
@@ -0,0 +1,30 @@
+﻿using HomeAddvisor.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HomeAddvisor.Controllers
+{
+    public class LoginController : Controller
+    {
+        private HOMEADDVISOR_DBEntities1 db = new HOMEADDVISOR_DBEntities1();
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
